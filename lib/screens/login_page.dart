d93c7cdcde1ff5f0331f203bc0e2b87a37565e35
Joao Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-10-27 10:33:06 -0300

    Adicionando Login Page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_message_app/assets/themes/app_colors.dart';
 import 'package:flutter_message_app/controllers/auth/login_controller.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_message_app/widgets/login_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: TextButton(
-              onPressed: () {
-                LoginController().googleSignIn(context);
-              },
-              child: Text('Login'),
-            ),
-          )
-        ],
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Container(
+          width: size.width * 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Choose a login option',
+                style: TextStyle(color: AppColors.white),
+              ),
+              LogInButton(
+                onTap: () {
+                  LoginController().googleSignIn(context);
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
