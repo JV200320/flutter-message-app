433e9d04baca15afd02c75fe743733ae1370a01b
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-27 11:53:57 -0300

    Resolver minor fixes

--- lib/widgets/login_button.dart
+++ lib/widgets/login_button.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter_message_app/assets/themes/app_colors.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
