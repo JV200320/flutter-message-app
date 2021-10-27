2a7d9093326d09a4cc6d2ef65f2a58a3717f0afd
Joao Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-10-27 10:09:56 -0300

    Adicionando paleta de cores

---
+++ lib/assets/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFF282828);
+  static const lighterBackground = Color(0xFF404040);
+  static const purple = Color(0xFF91229B);
+  static const grey = Color(0xFFC4C4C4);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+}
