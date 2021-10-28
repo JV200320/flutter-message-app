e65bd9ebcdad933441e80e9401c70fb440d92fff
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-28 14:27:10 -0300

    Adds Message Send Button

--- lib/widgets/message_input.dart
+++ lib/widgets/message_input.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_message_app/assets/themes/text_styles.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // -------------------------------------------------------------------------
 // Quando for chamar esse componente num StatefulWidget,
@@ -11,31 +12,47 @@ import 'package:flutter_message_app/assets/themes/text_styles.dart';
 
 class MessageInput extends StatelessWidget {
   final void Function(String value) onChanged;
-  final TextEditingController? controller;
+  final VoidCallback onPressed;
 
-  const MessageInput({Key? key, required this.onChanged, this.controller})
-      : super(key: key);
+  const MessageInput({
+    Key? key,
+    required this.onChanged,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Row(
-      children: [
-        Container(
-          width: size.width * 0.73,
-          height: size.width * 0.06,
-          child: TextFormField(
-              onChanged:
-                  onChanged, // Quando for instanciado, chama controller.OnChange
-              style: TextStyles.messageInput,
-              decoration: InputDecoration(
-                labelText: "Escreva sua mensagem aqui...",
-                labelStyle: TextStyles.messageInput,
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 17, horizontal: 25),
-              )),
-        )
-      ],
+    return Container(
+      height: size.height * 0.8,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: size.width * 0.73,
+              height: size.width * 0.06,
+              child: TextFormField(
+                onChanged:
+                    onChanged, // Quando for instanciado, chama controller.OnChange
+                style: TextStyles.messageInput,
+                decoration: InputDecoration(
+                  labelText: "Escreva sua mensagem aqui...",
+                  labelStyle: TextStyles.messageInput,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 17, horizontal: 25),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed:
+                  onPressed, // Aqui chama sendMessage de forma assincrona
+              child: Icon(FontAwesomeIcons.paperPlane),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
